c0108295deb7112c8f0a1da5843be97af4e13b4e
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-08-18 16:36:49 +0300

    config

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://waichinga:MainaMuiri@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
      # simple mde  configurations
@@ -23,8 +22,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace('://','ql://')
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://waichinga:MainaMuiri@localhost/pitch_test'
 
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export FLASK_APP=manage.py
-export SECRET_KEY='Mark Muiri'
+export SECRET_KEY='MarkMuiri'
 export MAIL_USERNAME='markwaichinga@gmail.com'
 export MAIL_PASSWORD='MainaMuiri'
 flask run
